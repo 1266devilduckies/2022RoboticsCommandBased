44354a1bfbb5f8e52c0fa9bc98098efe63162b2b
55sly <46945122+1266devilduckies@users.noreply.github.com>
2022-03-21 18:35:14 -0700

    managing changes 2

--- 2022RoboticsCommandBased/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ 2022RoboticsCommandBased/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,8 +7,14 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrainSubsystem extends SubsystemBase {
+  
+  private static DriveTrainSubsystem instance;
+  
   /** Creates a new DriverTrain Subsystem. */
   public DriveTrainSubsystem() {}
+
+  //all possible drivetrain function should be defined here
+  //DO NOT DEFINE COMMANDS HERE, ONLY SIMPLE FUNCTIONS
 
   @Override
   public void periodic() {
@@ -18,5 +24,12 @@ public class DriveTrainSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  public static synchronized DriveTrainSubsystem getInstance() {
+    if (instance == null) {
+      instance = new DriveTrainSubsystem();
+    }
+    return instance;
   }
 }
