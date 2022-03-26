946581b8e12123baf113ecbae04888d5f6f0b785
55sly <46945122+1266devilduckies@users.noreply.github.com>
2022-03-25 17:15:42 -0700

    removed extra limelight class

--- 2022RoboticsCommandBased/src/main/java/frc/robot/hardware/limelight/limeLightDataFetcher.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.hardware.limelight;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-public class limeLightDataFetcher {
-    
-}
